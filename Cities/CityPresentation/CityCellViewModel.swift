9e4fd1eade1dfca90625b9c1b0e3dee0a14499bf
Juan Francisco Bazan Carrizo <fran.bazan11@icloud.com>
2026-08-08 19:23:13 -0300

    Agregar CityCellViewModel con el titulo y el subtitulo de la celda

---
+++ Cities/CityPresentation/CityCellViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CityCellViewModel.swift
+//  Cities
+//
+//  Created by Juan Francisco Bazan Carrizo on 08/08/2026.
+//
+
+public struct CityCellViewModel: Equatable, Identifiable, Sendable {
+    public let id: Int
+    public let title: String
+    public let subtitle: String
+
+    public init(city: City) {
+        id = city.id
+        title = "\(city.name), \(city.countryCode)"
+        subtitle = "\(city.latitude), \(city.longitude)"
+    }
+}
